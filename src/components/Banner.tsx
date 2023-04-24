eb20be312acf4190da67e90967229288f843e202
Carlos Soto <carlos.m.soto2@gmail.com>
2023-04-24 17:29:17 -0400

    Added Header component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className='py-4 bg-gray-100 px-3'>
+    <div className='py-4 bg-gray-100 px-3 relative'>
       <p className='text-center text-xs tracking-[.1rem] font-normal uppercase ease-in-out text-[#5c5c5c]'>
         CERTIFIED AUTHENTIC | FREE INSURED SHIPPING ON ALL WATCHES |
         614-377-7216
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { BiMenuAltLeft, BiShoppingBag } from 'react-icons/bi';
 
 const Header = () => {
-  return <header>Header</header>;
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  const [isShoppingCartOpen, setIsShoppingCartOpen] = useState<boolean>(false);
+
+  return (
+    <header className='bg-white border-b top-0 left-0 sticky'>
+      <div className='flex justify-between py-4 px-[18px] items-center'>
+        <BiMenuAltLeft
+          size={36}
+          className='hover:cursor-pointer'
+          color='#666565'
+          onClick={() => setIsMenuOpen(true)}
+        />
+        <img
+          src={
+            'https://cdn.shopify.com/s/files/1/0530/2802/8612/files/final-25dials-favicon_75x.png?v=1613579839'
+          }
+          // change to nextjs image tag and change src to svg file david sends
+          alt='25 dials company logo'
+        />
+        <BiShoppingBag
+          size={36}
+          className='hover:cursor-pointer'
+          color='#666565'
+          onClick={() => setIsShoppingCartOpen(true)}
+        />
+      </div>
+
+      {/* open mobile menu */}
+      {/* will need to do same for shopping cart*/}
+      {isMenuOpen ? (
+        <div className='absolute top-0 left-0 bottom-0'>
+          <div className='bg-black'></div>
+          <div className='bg-white w-[90%]'>menu op</div>
+        </div>
+      ) : null}
+    </header>
+  );
 };
 
 export default Header;
