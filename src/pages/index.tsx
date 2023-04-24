f82ee28af63d3070914105af3c4a8e0dee019f58
Carlos Soto <carlos.m.soto2@gmail.com>
2023-04-24 00:40:39 -0400

    changed font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import { Jost } from 'next/font/google';
 import Header from '@/components/Header';
+import Banner from '@/components/Banner';
 
-const inter = Inter({ subsets: ['latin'] });
+const jost = Jost({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
+      <Banner />
       <Header />
       <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${jost.className}`}
       ></main>
     </>
   );
