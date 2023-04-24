e7052a9ec681430dc0911f0dbcf50d5379480508
Carlos Soto <carlos.m.soto2@gmail.com>
2023-04-24 00:40:22 -0400

    added banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <div className='py-4 bg-gray-100 px-3'>
+      <p className='text-center text-xs tracking-[.1rem] font-normal uppercase ease-in-out text-[#5c5c5c]'>
+        CERTIFIED AUTHENTIC | FREE INSURED SHIPPING ON ALL WATCHES |
+        614-377-7216
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
