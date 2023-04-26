83ca8242573e40efbdc53ce96569e37d572fd9d5
Ricardo Poullet <rickypoullet@gmail.com>
2023-04-26 02:47:33 -0400

    Created Product Item Comp. refactor MenuSlideIn, styled <main>

--- src/components/MenuSlideIn.tsx
+++ src/components/MenuSlideIn.tsx
@@ -1,22 +1,53 @@
-import Link from 'next/link';
-import { GrClose } from 'react-icons/gr';
-import { AiOutlineInstagram, AiFillYoutube } from 'react-icons/ai';
-import { FaFacebookF } from 'react-icons/fa';
+import Link from "next/link";
+import { GrClose } from "react-icons/gr";
+import { AiOutlineInstagram, AiFillYoutube } from "react-icons/ai";
+import { FaFacebookF } from "react-icons/fa";
 
 const MenuSlideIn = ({ handleMenuClose }: { handleMenuClose: () => void }) => {
+  type navlinkType = {
+    title: string;
+    href: string;
+  };
+
+  const navLinks = [
+    { title: "Home", href: "/" },
+    { title: "Soft Goods", href: "/" },
+    { title: "Sell your watch", href: "/" },
+    { title: "Sold watches", href: "/" },
+    { title: "Contact us", href: "/" },
+  ] as navlinkType[];
+
+  const socialLinks = [
+    {
+      icon: <FaFacebookF size={25} color={"#5c5c5c"} />,
+      href: "https://www.facebook.com/25Dials",
+      ariaLabel: "Link to 25 dials facebook page",
+    },
+    {
+      icon: <AiOutlineInstagram size={30} color={"#5c5c5c"} />,
+      href: "https://www.instagram.com/25dials/",
+      ariaLabel: "Link to 25 dials facebook page",
+    },
+    {
+      icon: <AiFillYoutube size={30} color={"#5c5c5c"} />,
+      href: "https://www.youtube.com/channel/UCdhDaXP0EapiBowzRyeoI9Q",
+      ariaLabel: "Link to 25 dials facebook page",
+    },
+  ];
+
   return (
     <section>
       <div
-        className='fixed bottom-0 left-0 right-0 top-0 z-40 h-screen w-full bg-black opacity-75'
+        className="fixed bottom-0 left-0 right-0 top-0 z-40 h-screen w-full bg-black opacity-75"
         onClick={handleMenuClose}
       />
-      <div className='fixed bottom-0 left-0 top-0 z-50 flex h-screen w-[90%] flex-col bg-white px-6 pt-6 ease-in'>
+      <div className="fixed bottom-0 left-0 top-0 z-50 flex h-screen w-[90%] flex-col bg-white px-6 pt-6 ease-in">
         <div
-          className='cursor-pointer'
-          aria-label='Click to close menu'
+          className="cursor-pointer"
+          aria-label="Click to close menu"
           tabIndex={0}
           onKeyDown={(e) => {
-            if (e.key === 'Enter') {
+            if (e.key === "Enter") {
               handleMenuClose();
             }
           }}
@@ -24,67 +55,29 @@ const MenuSlideIn = ({ handleMenuClose }: { handleMenuClose: () => void }) => {
           <GrClose size={20} onClick={handleMenuClose} />
         </div>
 
-        <div className='relative'></div>
-        <nav className='mt-5 flex flex-col'>
-          <Link
-            href='/'
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Home
-          </Link>
-          <Link
-            href=''
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Soft goods
-          </Link>
-          <Link
-            href=''
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Sell your watch
-          </Link>
-          <Link
-            href=''
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Sold watches
-          </Link>
-          <Link
-            href=''
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Contact us
-          </Link>
-          <Link
-            href=''
-            className='cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray'
-          >
-            Schedule Showing
-          </Link>
+        <div className="relative"></div>
+        <nav className="mt-5 flex flex-col">
+          {navLinks.map(({ title, href }, index) => (
+            <Link
+              href={href}
+              key={index}
+              className="cursor-pointer border-b py-5 text-[1rem] uppercase tracking-dials-spacing text-dials-gray"
+            >
+              {title}
+            </Link>
+          ))}
         </nav>
-        <div className='mt-auto flex items-center justify-between border-t py-2 '>
-          <Link
-            href={'https://www.facebook.com/25Dials'}
-            target='_blank'
-            aria-label='Link to 25 dials facebook page'
-          >
-            <FaFacebookF size={25} color={'#5c5c5c'} />
-          </Link>
-          <Link
-            href={'https://www.instagram.com/25dials/'}
-            target='_blank'
-            aria-label='Link to 25 dials instagram page'
-          >
-            <AiOutlineInstagram size={30} color={'#5c5c5c'} />
-          </Link>
-          <Link
-            href={'https://www.youtube.com/channel/UCdhDaXP0EapiBowzRyeoI9Q'}
-            target='_blank'
-            aria-label='Link to 25 dials youtube channel'
-          >
-            <AiFillYoutube size={30} color={'#5c5c5c'} />
-          </Link>
+        <div className="mt-auto flex items-center justify-between border-t py-2 ">
+          {socialLinks.map(({ icon, href, ariaLabel }, index) => (
+            <Link
+              key={index}
+              href={href}
+              target="_blank"
+              aria-label={ariaLabel}
+            >
+              {icon}
+            </Link>
+          ))}
         </div>
       </div>
     </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,45 @@
-import Image from 'next/image';
-import { Jost } from 'next/font/google';
-import Header from '@/components/Header';
-import Banner from '@/components/Banner';
+import Image from "next/image";
+import { Jost } from "next/font/google";
+import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+import ProductItem from "@/components/ProductItem";
 
-const jost = Jost({ subsets: ['latin'] });
+const jost = Jost({ subsets: ["latin"] });
+
+const itemsData = [{}, {}, {}];
 
 export default function Home() {
   return (
-    <div className={`${jost.className}`}>
+    <div className={`${jost.className} font-light tracking-[.1rem]`}>
       <Banner />
       <Header />
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+      <h1 className="font py-8 text-center text-[1.9rem] uppercase">Shop</h1>
+      <main className=" gap auo-rows-max xl:px grid min-h-screen grid-cols-2 gap-x-2.5 gap-y-8 px-4 lg:grid-cols-4 lg:gap-x-6 lg:px-6">
+        {itemsData.map((data, index) => (
+          <ProductItem
+            key={index}
+            href="/"
+            src="https://cdn.shopify.com/s/files/1/0530/2802/8612/products/DSCF1704_44a0b6ba-c46f-4d0b-9685-3e848bb84ff4_800x.jpg?v=1682356179"
+            price={1000}
+            title={"ROLEX DATEJUST 16013 CHAMPAGNE DIAMOND DIAL - 1984"}
+          />
+        ))}
+        <ProductItem
+          href="/"
+          src="https://cdn.shopify.com/s/files/1/0530/2802/8612/products/DSCF1704_44a0b6ba-c46f-4d0b-9685-3e848bb84ff4_800x.jpg?v=1682356179"
+          price={1000}
+          title={
+            "ROLEX DATEJUST 16013 CHAMPAGNE DIAMOND DIAL - 1984 qiwjidqihhai longer name"
+          }
+          soldOut
+        />
+        <ProductItem
+          href="/"
+          src="https://cdn.shopify.com/s/files/1/0530/2802/8612/products/DSCF1704_44a0b6ba-c46f-4d0b-9685-3e848bb84ff4_800x.jpg?v=1682356179"
+          price={1000}
+          title={"ROLEX DATEJUST 16013 CHAMPAGNE DIAMOND DIAL - 1984"}
+        />
+      </main>
     </div>
   );
 }
