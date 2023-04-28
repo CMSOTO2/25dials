23ca966c74efeda42aa145dea3503dc7c7451041
Carlos Soto <carlos.m.soto2@gmail.com>
2023-04-27 21:42:30 -0400

    added integration to swell

--- src/util/types.ts
+++ src/util/types.ts
@@ -2,3 +2,59 @@ export type navlinkType = {
   title: string;
   href: string;
 };
+
+export type productItemType = {
+  href: string;
+  src: string;
+  price: number;
+  title: string;
+  soldOut?: boolean;
+};
+
+export type SwellDataType = {
+  count: number;
+  page: number;
+  page_count: number;
+  results: ResultsType[];
+};
+
+export type ResultsType = {
+  attribues?: object;
+  bundle?: any;
+  currency: string;
+  description: string;
+  id: string;
+  images: SwellImagesType[];
+  meta_description: string | null;
+  meta_title: string | null;
+  name: string;
+  options?: any;
+  price: number;
+  purchase_options: PurchaseOptionsType;
+  sale: boolean;
+  sku: null | any;
+  slug: string;
+  stock_level: number;
+  stock_purchasable: boolean;
+  stock_status: string;
+  stock_tracking: boolean;
+  tags: any;
+};
+
+export type PurchaseOptionsType = {
+  price: number;
+  sale: boolean;
+  sale_price: null | any;
+};
+
+export type SwellImagesType = {
+  id: string;
+  file: SwellImagesFileType;
+};
+
+export type SwellImagesFileType = {
+  height: number;
+  md5: string;
+  url: string;
+  width: number;
+};
